4f554d7895781da190b434a6dd700915f775644e
Mike Bird <mikebird148@gmail.com>
2021-11-13 10:35:45 -0800

    updated the add place section of the application and added validation to the form

--- front-end/src/user/pages/Users.js
+++ front-end/src/user/pages/Users.js
@@ -8,7 +8,7 @@ const Users = () => {
       id: 'u1',
       name: 'Mike Bird',
       image:
-        'https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/123224209_10223724309119574_1122481359688748289_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=7gLcYX-HNTQAX9feT2R&_nc_ht=scontent-sea1-1.xx&oh=48ccebcb2a86edf511c1b424430e1da0&oe=61B3C299',
+        'https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/123224209_10223724309119574_1122481359688748289_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=xe14_h6eyBkAX---WZA&_nc_ht=scontent-sea1-1.xx&oh=798a1fb7d15a1906c07705cad03d4fa5&oe=61ABD999',
       places: 3
     }
   ];
