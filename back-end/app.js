72752764598dc08eaea1c286c152ce1628db6997
Mike Bird <mikebird148@gmail.com>
2021-11-14 16:26:42 -0800

    added routes, controllers and error handling for use with postman. Routes were made in order to test creating new users, logging in, adding users and places, getting existing places, updating current places and descriptions and deleting places. The google geocoding api was also incorporated into the back end in order to change an address into coordinates.

--- back-end/app.js
+++ back-end/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/places', placesRoutes); // -> /api/places/...
-// app.use('/api/users', usersRoutes); // -> /api/places/...
+app.use('/api/users', usersRoutes); // -> /api/places/...
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
